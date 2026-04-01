527ac7511ced177dbf1327fada17f720aeb4852b
paxington181 <tsandrus@rogers.com>
2026-04-01 11:20:26 -0400

    Building sets from JSON for tkinter use

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ import json
 
 def main():
 
+    all_monsters = set()
+    all_types = set()
+    all_terrain = set()
+    all_books = set()
+
     with open('monsters.json') as file:
         monsters = json.load(file)
-        print(monsters)
 
+    for monster_name, monster in monsters.items():
+        all_monsters.add(monster_name)
+        all_types.add(monster["type"])    
+        all_books.add(monster["book"])
+        for terrain in monster["terrain"]:
+            all_terrain.add(terrain)
+        
+    print(all_monsters)
+    print(all_types)
+    print(all_terrain)
+    print(all_books)
 
 main()
